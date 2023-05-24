d8a9b04935ee69081e2a4bf3dc2263f3ebe685ff
Morrison18 <35890626+Morrison18@users.noreply.github.com>
2023-05-24 20:04:17 +0100

    Create index.php
    Create repo

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+# Database Configuration
+define( 'DB_NAME', '' );
+define( 'DB_USER', '' );
+define( 'DB_PASSWORD', '' );
+define( 'DB_HOST', '' );
+define( 'DB_HOST_SLAVE', '' );
+define('DB_CHARSET', '');
+define('DB_COLLATE', '');
+$table_prefix = '';
+?>
